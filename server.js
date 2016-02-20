d0cf91712efcf2126ee8e54bc47202815f76052a
zharley18 <zackharley@gmail.com>
2016-02-20 13:45:53 -0500

    Add node modules for request and Smooch. Have a basic request for the Google homepage

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var Smooch = require('smooch');
+
+Smooch.init({
+    appToken: '2daitql57gg85z3jurs6g16xf'
+});
 
 var port = process.env.PORT || 1337;
 
+// Routing to the user
+app.use(express.static(__dirname + "/public"));
 
- 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+    request('http://www.google.com', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body) // Show the HTML for the Google homepage. 
+        }
+    })
 })
- 
-app.listen(port, function() {
+
+app.listen(port, function () {
     console.log("Server is listening on port " + port.toString());
 });
