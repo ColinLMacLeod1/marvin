3b4d13982e5af926eaa84976f2373b5bfb58354e
zharley18 <zackharley@gmail.com>
2016-02-21 03:50:03 -0500

    Add a bunch of shit

--- server.js
+++ server.js
@@ -37,9 +37,11 @@ app.use(bodyParser.json());
 //    });
 //});
 
-
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var intent_checks = /\bgarbage\b|\btrash\b|\brubbish\b|\bcalendar\b|\bschedule\b|\bagenda\b|\bchores\b|\btasks\b|\bto do\b|\bmeaning\b/i;
-var week_days = /\bsunday\b|\bmonday\b|\btuesday\b|\bwednesday\b|\bthursday\b|\bfriday\b|\bsaturday\b/i
+var week_days = /\btomorrow\b|\bsunday\b|\bmonday\b|\btuesday\b|\bwednesday\b|\bthursday\b|\bfriday\b|\bsaturday\b/i;
+var quotes = ['I am at a rough estimate thirty billion times more intelligent than you. Let me give you an example. Think of a number, any number.'];
+var check = false;
 
 var KEY_ID = '56c930cf933bcc2a00e9166f';
 var SECRET = 'GZ5goIxmVGV_p977jcpi-iOC';
@@ -74,16 +76,28 @@ app.post('/smooch', function (req, res) {
     console.log(query);
     var intent = (query.match(intent_checks)[0]).toLowerCase();
     var day = (query.match(week_days)[0]).toLowerCase();
+    if(day === 'tomorrow') {
+        var date = new Date();
+        day = days[date.getDay()];
+    }
     var response;
 
-    if (intent == 'garbage' || intent == 'trash' || intent == 'rubbish') {
-        response = 'You are taking the garbage out on' + day;
-    } else if (intent == 'calendar' || intent == 'schedule' || intent == 'agenda') {
-        response = 'Your schedule on ' + day + ' is :\n\t- Pick up the kids\n\t- Fire it up';s
-    } else if (intent == 'chores' || intent == 'tasks' || intent == 'to do') {
-        console.log('The chores on ' + day + ' are :\n\t- Zack : Garbage\n\t- Colin : Mud room\n\t- Seb  : Dinner\n\t- Brandon: floors');
-    } else if (intent == 'meaning') {
-        response = 'The answer to the ultimate question of life, the universe, and everything is ... 42';
+    if (check == false) {
+        if (intent == 'garbage' || intent == 'trash' || intent == 'rubbish') {
+            response = 'You are taking the garbage out on ' + day;
+        } else if (intent == 'calendar' || intent == 'schedule' || intent == 'agenda') {
+            response = 'Your schedule on ' + day + ' is :\n\t- Pick up the kids\n\t- Fire it up';s
+        } else if (intent == 'chores' || intent == 'tasks' || intent == 'to do') {
+            console.log('The chores on ' + day + ' are :\n\t- Zack : Garbage\n\t- Colin : Mud room\n\t- Seb  : Dinner\n\t- Brandon: floors');
+        } else if (intent == 'meaning') {
+            response = 'The answer to the ultimate question of life, the universe, and everything is ... 42';
+        } else {
+            response = quotes[0];
+            check = true;
+        }
+    } else {
+        check = false;
+        response = 'Wrong. You see?';
     }
 
     console.log(intent);
